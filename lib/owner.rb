55ecd584742a69c9b335c4e3741119a4266bc43f
Mika88 <mikaspivak@gmail.com>
2018-08-09 17:56:10 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -52,6 +52,6 @@ class Owner
     self.pets[:fishes].each{|fish| fish.mood = "happy"}
   end
   def sell_pets
-       self.pets.values.each{|pet| pet.mood = "nervous"}.clear
+       self.pets.values
   end
 end
